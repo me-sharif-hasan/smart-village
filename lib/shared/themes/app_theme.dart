ac33cd85757a84960409b22201ce79fae00f2488
Md. Shamim Miah <shamim.miah@brainstation-23.com>
2025-05-27 16:47:31 +0600

    refactor: update cardTheme to use CardThemeData for improved theming

--- lib/shared/themes/app_theme.dart
+++ lib/shared/themes/app_theme.dart
@@ -32,7 +32,7 @@ class AppTheme {
       elevation: 0,
       centerTitle: true,
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       color: cardBackground,
       elevation: 2,
       shape: RoundedRectangleBorder(
